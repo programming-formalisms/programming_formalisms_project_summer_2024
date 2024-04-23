768a7abe178dd93efe84503fb36c4461c37825e4
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-04-23 15:31:37 +0200

    Richel is done

--- learners/richel/is_odd.py
+++ learners/richel/is_odd.py
@@ -1,8 +1,20 @@
 
 def is_zero(x):
-  """srgiupgwiuwfeuhgb;"""
+  """
+  Returns true if x is one integer that is zero.
+  Raises an exception when x is not an integer.
+  """
+  if not isinstance(x, int):
+      raise TypeError("x must be one integer")
   return x == 0
 
 assert is_zero.__doc__
 assert is_zero(0)
 assert not is_zero(1)
+
+has_thrown = False
+try:
+    is_zero("wiefgoweubfipf")
+except TypeError:
+    has_thrown = True
+assert has_thrown
