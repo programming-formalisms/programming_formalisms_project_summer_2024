c7c57efa3f99c03a61027ea6263ac35003e49a41
Hyunho Kim <hyunho.kim@dbb.su.se>
2024-04-23 15:41:54 +0200

    addded doc

--- learners/phuong/is_zero.py
+++ learners/phuong/is_zero.py
@@ -1,4 +1,5 @@
 def is_zero(x):
+    """ Definition to check if int is zero """
     if not isinstance(x, int):
         raise TypeError("'x' must be of type int")
     if x == 0:
@@ -7,3 +8,6 @@ def is_zero(x):
 
 
 assert is_zero(0)
+assert not is_zero(1)
+assert is_zero.__doc__
+assert is_zero("awdawd")
