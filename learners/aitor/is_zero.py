c80ce2bf430522b371cfadad09e82d1c621248e1
AitorAldama <aitor.aldamacampino@smhi.se>
2024-04-23 15:37:14 +0200

    'stuff' is not zerro'

--- learners/aitor/is_zero.py
+++ learners/aitor/is_zero.py
@@ -1,6 +1,17 @@
 def is_zero(val):
+
+    if not isinstance(val,int):
+        raise TypeError(f"{val} is not a integer")
     if val == 0:
         return True
         
 assert is_zero(0)
-assert is_zero("Stuff")
+has_thrown = False
+try:
+    is_zero("Stuff")
+except TypeError:
+    has_thrown = True
+
+assert has_thrown
+
+assert is_zero(1)
