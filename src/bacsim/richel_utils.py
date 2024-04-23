a715156e3efce5ad56a68d5531801558c1fd9ec5
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-04-23 21:02:13 +0200

    Add first testing function

---
+++ src/bacsim/richel_utils.py
@@ -0,0 +1,16 @@
+"""Richel's utility functions"""
+
+def is_zero(x):
+    """Determine if `x` is zero.
+
+    If `x` is not a number, a `TypeError` is raised.
+
+    Returns `True` if `x` is zero
+    """
+    if not isinstance(x, (int, float)):
+        msg = "'number' must be a number. "
+        raise TypeError(
+            msg,
+            "Actual type of 'number': ", type(x),
+        )
+    return x == 0
---
+++ tests/test_richel_utils.py
@@ -0,0 +1,18 @@
+"""Tests all function in src.pf_example.easy_solutions."""
+import unittest
+
+from src.bacsim.richel_utils import is_zero
+
+class TestRichelUtils(unittest.TestCase):
+
+    """Class to test the functions in src.bacsim.richel_utils."""
+
+    def test_is_zero(self):
+        """Test 'is_zero'."""
+        self.assertIsNotNone(is_zero.__doc__)
+        self.assertTrue(is_zero(0))
+        self.assertTrue(is_zero(0.0))
+        self.assertFalse(is_zero(1))
+        self.assertRaises(TypeError, is_zero, {1, 2})
+        self.assertRaises(TypeError, is_zero, "I am a string")
+
