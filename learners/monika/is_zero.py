b8b3a5bc1396e59c5d162c1792eb50899b7ce139
Monika Brandt <monika.brandt@medsci.uu.se>
2024-04-23 15:47:23 +0200

    is zero function updates

--- learners/monika/is_zero.py
+++ learners/monika/is_zero.py
@@ -1,5 +1,7 @@
 def is_zero(x):
     """Determines if the input is one integer that is zero"""
+    if not isinstance(x, int):
+        raise TypeError("'x' must be of type int")
     if x == 0:
         return True
     return False
@@ -7,5 +9,12 @@ def is_zero(x):
 assert is_zero.__doc__
 assert is_zero(0)
 assert not is_zero(1)
+
+has_thrown = False
+try:
+    is_zero("nonsense")
+except TypeError:
+    has_thrown = True
+assert has_thrown
 
 
