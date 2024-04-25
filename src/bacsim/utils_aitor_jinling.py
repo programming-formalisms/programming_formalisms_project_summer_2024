946b805fb960f8264096f954b23a265ee2bb4a0d
AitorAldama <aitor.aldamacampino@smhi.se>
2024-04-25 09:25:22 +0200

    Test solved for integers

--- src/bacsim/utils_aitor_jinling.py
+++ src/bacsim/utils_aitor_jinling.py
@@ -1,5 +1,9 @@
 def is_even(x):
     """this is a function of is even"""
-    return True 
+    return x%2 == 0
+
 assert is_even(0)
 assert not is_even(1)
+assert is_even.__doc__
+
+assert is_even(2.4)
