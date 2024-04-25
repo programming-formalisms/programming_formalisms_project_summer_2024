d6409c9ef13ff07466aac3cffab969b2fb81614f
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2024-04-25 11:40:12 +0200

    Change position

---
+++ tests/test_Position.py
@@ -0,0 +1,13 @@
+"""Tests Position class"""
+
+import unittest
+
+from src.bacsim.Position import Position
+
+class TestPosition(unittest.TestCase):
+
+    """Class to test Position class"""
+
+    def test_Position(self):
+        self.assertIsNone(Position)
+
