b3983acf8309b3043318b48c7c14d5a7740cdb55
Karthik Nair <karthik.nair1194@gmail.com>
2024-04-25 09:58:17 +0200

    TypeError Tests

--- tests/test_utils_aitor_jinling.py
+++ tests/test_utils_aitor_jinling.py
@@ -1,11 +1,15 @@
 import unittest
 
 def is_probability(x):
-    return True
+    return(x >= 0 and x <= 1)
 
 class Test(unittest.TestCase):
     def test_is_true(self):
         self.assertTrue(is_probability(0))
 
     def test_is_false(self):
-        self.assertFalse(is_probability(0))
+        self.assertFalse(is_probability(2))
+    
+    def test_is_not_number(self):
+        self.assertRaises(TypeError, is_probability("TestString"))
+
