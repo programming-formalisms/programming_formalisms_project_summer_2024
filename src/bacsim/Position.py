4e14f2dbfbcef1fb0eeaf4cbcd9a8e734329bf15
phuongcaothingoc <ctnphuong@gmail.com>
2024-04-25 11:52:09 +0200

    issue 36

--- src/bacsim/Position.py
+++ src/bacsim/Position.py
@@ -1,8 +1,12 @@
 """A coordinate somewhere in space."""
 
-class Position:{
+class Position:
     """- int x
     - int y
     - init()"""
-}
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+
   
