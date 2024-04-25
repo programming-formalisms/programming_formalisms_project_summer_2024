01e0935ff8d506cb92233a0eb0e93b706f2c34fa
phuongcaothingoc <ctnphuong@gmail.com>
2024-04-25 11:59:34 +0200

    add issue 36 att. x, y

--- src/bacsim/Position.py
+++ src/bacsim/Position.py
@@ -4,6 +4,8 @@ class Position:
     """- int x
     - int y
     - init()"""
+    x=3
+    y=10
     def __init__(self,x,y):
         self.x=x
         self.y=y
--- tests/test_Position.py
+++ tests/test_Position.py
@@ -7,7 +7,7 @@ from src.bacsim.Position import Position
 class TestPosition(unittest.TestCase):
 
     """Class to test Position class"""
-
+    
     def test_Position(self):
         self.assertIsNotNone(Position)
         self.assertIsNotNone(Position.x)
