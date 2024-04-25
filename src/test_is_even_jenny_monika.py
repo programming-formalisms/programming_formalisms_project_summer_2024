0ef3dafaad0e41fe227ea56940ddff792fe87c3b
Monika Brandt <monika.brandt@medsci.uu.se>
2024-04-25 09:47:31 +0200

    Another failed test

--- src/test_is_even_jenny_monika.py
+++ src/test_is_even_jenny_monika.py
@@ -4,5 +4,11 @@ from is_even_jenny_monika import is_even
 
 class TestIsEven(unittest.TestCase):
 
-    def test_is_even(self):
+    def test_is_even_true(self):
         self.assertEqual(is_even(2), True)
+
+    def test_is_even_false(self):
+        self.assertEqual(is_even(3), False)
+
+    def test_is_even_empty(self):
+        self.assertEqual(is_even(), "Warning! The input must be an integer")
