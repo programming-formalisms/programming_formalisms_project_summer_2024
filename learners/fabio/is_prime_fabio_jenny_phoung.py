e4d087b321f21be115577cbd38f62f54cf9c090d
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2024-04-25 15:49:47 +0200

    Fixed is_prime lent

--- learners/fabio/is_prime_fabio_jenny_phoung.py
+++ learners/fabio/is_prime_fabio_jenny_phoung.py
@@ -1,8 +1,5 @@
-import unittest
+"""Test if number is prime."""
 
 def is_prime(n):
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
-            return False
-        
-    return True
+    """Function to test if a number is prime."""
+    return all(n % i != 0 for i in range(2, int(n ** 0.5) + 1))     
