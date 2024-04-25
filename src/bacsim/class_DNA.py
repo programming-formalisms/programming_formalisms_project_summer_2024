fbd8d502cdabcbd8409f65ec70578506e54a915a
phuongcaothingoc <ctnphuong@gmail.com>
2024-04-25 13:48:21 +0200

    add test issue_23

--- src/bacsim/class_DNA.py
+++ src/bacsim/class_DNA.py
@@ -4,8 +4,13 @@ class DNA:
     """
     this class will randomly select A-T and G-C nucleotide pairs.
     """
-        
+    def DNA_string():
+        return "catgc"
 
 class DNATest(unittest.TestCase):
     def test_is_DNA_string(self):
-        self.assertIsInstance(DNA_string(), str)
+        self.assertIsInstance(DNA.DNA_string(), str)
+
+    def test_is_DNA_notNone(self): 
+        self.assertIn("t",DNA.DNA_string(3))
+
