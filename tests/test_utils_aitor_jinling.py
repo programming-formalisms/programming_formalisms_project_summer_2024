dc10cd12051aeaeb272a909d72a32b3c010c60ad
Jinlinghuang <jinling.huang.6236@student.uu.se>
2024-04-25 10:05:42 +0200

    add instance check

--- tests/test_utils_aitor_jinling.py
+++ tests/test_utils_aitor_jinling.py
@@ -1,15 +1,16 @@
 import unittest
 
 def is_probability(x):
+    if not isinstance(x,float):
+        raise TypeError("not a number")
     return(x >= 0 and x <= 1)
 
 class Test(unittest.TestCase):
     def test_is_true(self):
-        self.assertTrue(is_probability(0))
+        self.assertTrue(is_probability(0.0))
 
     def test_is_false(self):
-        self.assertFalse(is_probability(2))
+        self.assertFalse(is_probability(2.0))
     
     def test_is_not_number(self):
         self.assertRaises(TypeError, is_probability("TestString"))
-
