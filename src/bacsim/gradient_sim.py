7683d600a4ff3a7d6d870cd13d9c2916ea4d6047
Karthik Nair <karthik.nair1194@gmail.com>
2024-04-25 11:50:56 +0200

    Added test

--- src/bacsim/gradient_sim.py
+++ src/bacsim/gradient_sim.py
@@ -1,3 +1,9 @@
+import unittest
+
 class GradientSim:
     """This class is simulates gradients"""
     pass
+
+class GradientTest(unittest.TestCase):
+    def is_GradientSim(self):
+        self.assertTrue(GradientSim())
