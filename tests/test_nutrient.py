ab5f298777ba474b57556d8b307242d15b880781
AitorAldama <aitor.aldamacampino@smhi.se>
2024-04-25 13:42:48 +0200

    First draft for test nutrient

---
+++ tests/test_nutrient.py
@@ -0,0 +1,11 @@
+"""Test for nutrient"""
+
+import unittest
+
+from src.bacsim.nutrient import Nutrient
+
+class TestNutrients(unittest.TestCase):
+
+    """Class to test the functions in src.bacsim.nutrient"""
+
+    
