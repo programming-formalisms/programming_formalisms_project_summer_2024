e0562beb61a971a812e863cf5606929f6eea7ea8
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2024-04-25 09:43:22 +0200

    first success

---
+++ src/bacsim/fabio_cecilia_utils.py
@@ -0,0 +1,4 @@
+# Utils 
+def is_prime_fabio_cecilia(x):
+    """ This is our function"""
+    return True
--- tests/test_fabio_cecilia.py
+++ tests/test_fabio_cecilia.py
@@ -10,7 +10,7 @@ import unittest
 from src.bacsim.fabio_cecilia_utils import is_prime_fabio_cecilia
 
 class TestIsPrime(unittest.TestCase):
-    def test_is_prime(self):
-        self.assertIsNotNone(is_prime.__doc__)
+    def test_is_prime_fabio_cecilia(self):
+        self.assertIsNotNone(is_prime_fabio_cecilia.__doc__)
 
         """Tests all function in src.bacsim.fabio_cecelia_utils."""
