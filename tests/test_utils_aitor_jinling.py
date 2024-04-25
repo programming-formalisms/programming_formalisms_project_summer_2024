8a2d42305a38472c10afed86458196cba068a3a4
AitorAldama <aitor.aldamacampino@smhi.se>
2024-04-25 09:43:24 +0200

    Second test failed

--- tests/test_utils_aitor_jinling.py
+++ tests/test_utils_aitor_jinling.py
@@ -5,7 +5,7 @@ def is_probability(x):
 
 class Test(unittest.TestCase):
     def test_is_true(self):
-        self.assertIsTrue(is_probability(0))
+        self.assertTrue(is_probability(0))
 
     def test_is_false(self):
-        self.assertIsTrue(not is_probability(2))
+        self.assertFalse(is_probability(0))
