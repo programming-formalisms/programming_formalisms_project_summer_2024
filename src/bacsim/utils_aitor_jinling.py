70e47f123674846b5d9715614ad307801d922395
Jinlinghuang <jinling.huang.6236@student.uu.se>
2024-04-25 09:30:17 +0200

    add integer check

--- src/bacsim/utils_aitor_jinling.py
+++ src/bacsim/utils_aitor_jinling.py
@@ -1,9 +1,12 @@
 def is_even(x):
     """this is a function of is even"""
+    if not isinstance(x,int):
+        raise TypeError("not a integer")
     return x%2 == 0
+    
 
 assert is_even(0)
 assert not is_even(1)
 assert is_even.__doc__
 
-assert is_even(2.4)
+assert is_even(2.4) 
