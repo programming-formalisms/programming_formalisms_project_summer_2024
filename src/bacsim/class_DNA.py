b065186000f88d4695ba232e1e4b70b465de4446
Monika Brandt <monika.brandt@medsci.uu.se>
2024-04-25 14:03:08 +0200

    Another failed test #23

--- src/bacsim/class_DNA.py
+++ src/bacsim/class_DNA.py
@@ -4,13 +4,15 @@ class DNA:
     """
     this class will randomly select A-T and G-C nucleotide pairs.
     """
-    def DNA_string():
-        return "catgc"
+    def DNA_string(x):
+        return x
 
 class DNATest(unittest.TestCase):
     def test_is_DNA_string(self):
-        self.assertIsInstance(DNA.DNA_string(), str)
+        self.assertIsInstance(DNA.DNA_string("t"), str)
 
     def test_is_DNA_notNone(self): 
         self.assertIn("t",DNA.DNA_string(3))
 
+    def test_is_DNA_base():
+        self.assertEqual(DNA.DNA_string(3),("A-T" or "G-C"))
