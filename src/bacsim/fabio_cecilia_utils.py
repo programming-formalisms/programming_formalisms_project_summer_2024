7378417cd1b6b75bd8c0239989935b997c912db3
Cecilia Casadei <ceccas@w-ceccas-pc-0.local>
2024-04-25 10:08:21 +0200

    implemented function

--- src/bacsim/fabio_cecilia_utils.py
+++ src/bacsim/fabio_cecilia_utils.py
@@ -1,10 +1,16 @@
 # Utils 
 def is_prime_fabio_cecilia(x):
     """ This is our function"""
-    is_prime_out = False
-    for i in range(2, x):
-        if x % i == 0:
-            continue
-        if i == x:
-            is_prime_out = True 
+    is_prime_out = True
+    
+    if x == 1:        
+        return is_prime_out
+    
+    
+    else:
+        for i in range(2, x):
+            if x % i == 0:
+                is_prime_out = False
+                return is_prime_out
+            
     return is_prime_out
