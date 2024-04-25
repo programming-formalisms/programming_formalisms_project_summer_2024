8d8a7bf87f061cc2ac66a4f447216421adca6c95
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2024-04-25 09:57:09 +0200

    function works

--- src/bacsim/fabio_cecilia_utils.py
+++ src/bacsim/fabio_cecilia_utils.py
@@ -1,4 +1,10 @@
 # Utils 
 def is_prime_fabio_cecilia(x):
     """ This is our function"""
-    return True
+    is_prime_out = False
+    for i in range(2, x):
+        if x % i == 0:
+            continue
+        if i == x:
+            is_prime_out = True 
+    return is_prime_out
