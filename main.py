70355d4973be87ac6fcd3235a6e931196e277428
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-04-26 08:50:12 +0200

    Main works

--- main.py
+++ main.py
@@ -4,55 +4,35 @@ Project used in the UPPMAX Programming Formalisms Summer 2024 course.
 """
 
 import cProfile
-import sys
 
-from bacsim.simulation_terminal_controller import (
-    SimulationTerminalController,
+from bacsim.simulation import (
+  create_experiment,
+  create_parameters,
+  read_parameters_from_file,
+  run,
+  save,
 )
-from bacsim.simulation_window_controller import (
-    SimulationWindowController,
-)
 
-"""
-TODO
-
-
-from pf_example.testing import (
-    get_datas,
-    get_sorting_functions,
-    get_speed_measurements,
-    save_speed_measurements,
-)
-"""
+def do_workflow():
+    parameters = read_parameters_from_file("parameters.txt")
+    experiment = create_experiment(parameters)
+    results = run(experiment)
+    save(results, "results.csv")
 
 def do_benchmark():
     """Benchmark this project."""
-    cProfile.run(
-        "run_profile_simulation()",
-    )
-"""
-TODO
-    speed_measurements = get_speed_measurements(
-        datas = get_datas(data_lengths = [2, 4, 6, 8, 10]),
-        functions = get_sorting_functions(),
-    )
-    save_speed_measurements(speed_measurements, "speeds.csv")
-
-
-"""
+    cProfile.run("do_workflow()")
 
 if __name__ == "__main__":
 
-    if "--gui" in sys.argv:
-        print("GUI application") # noqa: T201 print is used as a stub
-        c = SimulationWindowController()
-        c.run()
-    elif "--benchmark" in sys.argv:
+    if "--benchmark" in sys.argv:
         print("Benchmarking application") # noqa: T201 print is used as a stub
         if __debug__:
             e = RuntimeError("Do not benchmark in debug mode")
             raise e
         do_benchmark()
     else:
-        print("Console application") # noqa: T201 print is used as a stub
-        c = SimulationTerminalController()
+        print("Running workflow") # noqa: T201 print is used as a stub
+        do_workflow()
+
+
