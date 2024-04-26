22220b666a9e26526c04a429d76a3ed0f3c3db8d
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-04-26 13:08:22 +0200

    Fix ruff

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -16,24 +16,29 @@ class TestExperiment(unittest.TestCase):
     """Class to test the code in src.bacsim.particle."""
 
     def test_create_experiment(self):
-        # Should pass without error
+        """"Create a default experiment."""
         create_experiment()
     def test_run(self):
+        """"Run a default experiment."""
         experiment = create_experiment()
         # Should pass without error
         run(experiment)
     def test_run_creates_a_result(self):
+        """"Get results from a run."""
         experiment = create_experiment()
         # Should pass without error
         results = run(experiment)
     def test_save(self):
+        """"Save results from a run."""
         experiment = create_experiment()
         results = run(experiment)
         # Should pass without error
         save(results, "my_results.csv")
     def test_create_an_experiment_with_parameters(self):
+        """"An experiment has parameters."""
         parameters = create_parameters()
         experiment = create_experiment(parameters)
     def test_read_parameters_from_file(self):
+        """"Parameters can be read from file."""
         # Should pass without errors
         read_parameters_from_file("parameters.txt")
